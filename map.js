5ae155d624a8d50a2058ca3559d13999896a42f3
Pedro Fuentes <pedro_herrera@hotmail.com>
2021-08-27 10:10:29 -0300

    xxx

---
+++ map.js
@@ -0,0 +1,29 @@
+const numbers = [1, 2, 3 , 4, 5]
+
+const numbers2 = numbers.map(function(el) {
+    return el *2
+})
+
+// outra versao .... alterado..
+const numbers21 = numbers.map(el => el *2)
+
+console.log(numbers2)
+
+const students = [
+    {name:'Jake', score: 6.4},
+    {name:'Suzan', score: 8.6},
+    {name:'Emma', score: 9.4},
+    {name:'Peter', score: 9.1},
+]
+
+const getScore = el =>el.score
+console.log(students.map(getScore))
+// outra versao
+console.log(students.map(el => el.score))
+
+console.log(
+    students
+        .map(getScore)
+        .map(Math.ceil)
+)
+
--- notes.md
+++ notes.md
@@ -3,5 +3,7 @@ untracked -- arquivos nao rastreados (novos)
 staged(preparado) -- foi marcado para realizar commit por git add
 committed 
 
-git commit -ma ' test'  --- faz um add . e depois commit 
-.
+git commit -ma ' test'  --- faz um add arq rastreados e depois commit 
+git rm arq   -- remove o arquivo e git faz add automaticamente para o staged 
+    caso tenha sido alterado alteriormente e staged,  e necessario git rm -f arq  para forçar o delete
+ 
